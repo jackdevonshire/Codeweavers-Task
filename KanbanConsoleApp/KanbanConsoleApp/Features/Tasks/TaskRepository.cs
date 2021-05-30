8a3812f0a4f878ac49e9d6ca66a08cdf612fc439
Jack Devonshire <itunes.jd@hotmail.com>
2021-05-30 21:32:21 +0100

    Added Second Task Base

---
+++ KanbanConsoleApp/KanbanConsoleApp/Features/Tasks/TaskRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace KanbanConsoleApp.Features.Tasks
+{
+    public class TaskRepository
+    {
+        private readonly List<Task> _tasks = new List<Task>();
+
+        public void SaveTask(Task task)
+        {
+            _tasks.Add(task);
+        }
+
+        public List<Task> GetTasks()
+        {
+            var tasksToReturn = new List<Task>();
+
+            foreach (var task in _tasks)
+            {
+                tasksToReturn.Add(new Task
+                {
+                    Id = task.Id,
+                    Description = task.Description
+                });
+            }
+
+            return tasksToReturn;
+        }
+    }
+}
---
+++ KanbanConsoleApp/KanbanConsoleApp/TaskEngine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using KanbanConsoleApp.Features.Clients;
+using KanbanConsoleApp.Features.Tasks;
+
+namespace KanbanConsoleApp
+{
+    public class TaskEngine
+    {
+        private readonly TaskRepository _taskRepository = new TaskRepository();
+
+
+        public void SaveTask(Task task)
+        {
+            _taskRepository.SaveTask(task);
+        }
+
+        public Task RetrieveTaskById(string id)
+        {
+            var allTasks = _taskRepository.GetTasks();
+            foreach (var task in allTasks)
+            {
+                return task;
+            }
+
+            return null;
+        }
+
+        public void SaveClient(Client client)
+        {
+        }
+
+        public List<Client> GetClients()
+        {
+            return new List<Client>();
+        }
+
+        public void RemoveClientByName(string name)
+        {
+        }
+
+        public List<Task> GetTasksForClient(string clientName)
+        {
+            return new List<Task>();
+        }
+    }
+}
