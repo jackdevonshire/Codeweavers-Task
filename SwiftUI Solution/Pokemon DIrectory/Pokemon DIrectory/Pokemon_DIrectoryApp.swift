e93dcfac813dd6f477df4721e552262f7af87e05
Jack Devonshire <git@itsjack.dev>
2021-05-30 13:10:30 +0100

    Base App Structure

---
+++ SwiftUI Solution/Pokemon DIrectory/Pokemon DIrectory/Pokemon_DIrectoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_DIrectoryApp.swift
+//  Pokemon DIrectory
+//
+//  Created by Jack Devonshire on 30/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_DIrectoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI Solution/Pokemon DIrectory/Pokemon DIrectory/Views/DirectoryView.swift
@@ -0,0 +1,20 @@
+//
+//  DirectoryView.swift
+//  Pokemon DIrectory
+//
+//  Created by Jack Devonshire on 30/05/2021.
+//
+
+import SwiftUI
+
+struct DirectoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DirectoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        DirectoryView()
+    }
+}
