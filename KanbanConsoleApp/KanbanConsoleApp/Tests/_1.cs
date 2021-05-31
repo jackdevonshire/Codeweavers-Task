e148c5adb8f3c96f22b40c8c75d71cf3f37928f4
Jack Devonshire <itunes.jd@hotmail.com>
2021-05-31 11:01:27 +0100

    Enabled Test One
    Altered Test One slightly. Now creates new Client and then inserts Client Name when creating new tasks, so that it passes the criteria implemented to pass test 6.

--- KanbanConsoleApp/KanbanConsoleApp/Tests/_1.cs
+++ KanbanConsoleApp/KanbanConsoleApp/Tests/_1.cs
@@ -1,4 +1,5 @@
 ﻿using KanbanConsoleApp.Features.Tasks;
+using KanbanConsoleApp.Features.Clients;
 
 namespace KanbanConsoleApp.Tests
 {
@@ -7,11 +8,12 @@ namespace KanbanConsoleApp.Tests
         public static void AbilityToGetATaskById_ThenTheCorrectTaskIsRetrieved()
         {
             var engine = new TaskEngine();
-            //engine.SaveTask(new Task { Description = "Ability to login to the order system.", Id = "1" });
-            //engine.SaveTask(new Task { Description = "Ability to logout of the order system.", Id = "2" });
+            engine.SaveClient(new Client { Name = "BMW" });
+            engine.SaveTask(new Task { Description = "Ability to login to the order system.", Id = "1", ClientName = "BMW" });
+            engine.SaveTask(new Task { Description = "Ability to logout of the order system.", Id = "2", ClientName = "BMW" });
 
-            //var result = engine.RetrieveTaskById("2");
-            //Assert.AreEqual(result.Description, "Ability to logout of the order system.");
+            var result = engine.RetrieveTaskById("2");
+            Assert.AreEqual(result.Description, "Ability to logout of the order system.");
         }
     }
 }
